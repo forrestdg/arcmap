29ea702e90bd08399cf428859b21e32804e25b3a
Duong Dang <dang.duong@gmail.com>
2010-12-30 16:37:22 +0100

    Latest modify on win machine.

--- compute.sql
+++ compute.sql
@@ -5,8 +5,9 @@ c_val real
 );
 
 /* Set alpha value here */
-INSERT INTO constants VALUES("alpha", -(4.0/3600.0));
-INSERT INTO constants VALUES("Tmax", 7200 );
+INSERT INTO constants VALUES("beta", -(3.0/60.0));
+INSERT INTO constants VALUES("Tmax", 120.0 );
+INSERT INTO constants VALUES("alpha", 0.2);
 
 
 /*import table*/
@@ -24,7 +25,9 @@ CREATE TABLE communes(
        Ind_job_co integer,
        Trade_job_ integer,
        Total_job_ integer ,
-       ORIG_FID integer);
+       ORIG_FID integer,
+	   labour integer
+	   );
 
 .separator ","
 SELECT "importing job_commne.csv";
@@ -41,9 +44,9 @@ sum_weighted_jobs=sigma(jobs*exp(-0.829*access_time))*/
 ALTER TABLE communes ADD COLUMN sum_weighted_jobs REAL;
 ALTER TABLE communes ADD COLUMN sum_weighted_labours REAL;
 ALTER TABLE communes ADD COLUMN competition_factor REAL;
-ALTER TABLE communes ADD COLUMN accessibility REAL;
-
-/*import accessibility table*/
+ALTER TABLE communes ADD COLUMN accessibility_alpha REAL;
+ALTER TABLE communes ADD COLUMN accessibility_beta  REAL;
+/*import accessibility_alpha table*/
 CREATE TABLE access (
        road_id integer,
        name text,
@@ -61,7 +64,9 @@ DELETE FROM access WHERE rowid = 1;
 ALTER TABLE access ADD COLUMN dest_jobs INTEGER;
 ALTER TABLE access ADD COLUMN dest_labours REAL;
 ALTER TABLE access ADD COLUMN time_alpha REAL;
+ALTER TABLE access ADD COLUMN time_beta REAL;
 ALTER TABLE access ADD COLUMN dest_weighted_jobs REAL;
+ALTER TABLE access ADD COLUMN dest_weighted_jobs_beta REAL;
 ALTER TABLE access ADD COLUMN dest_weighted_labours REAL;
 ALTER TABLE access ADD COLUMN accessibilty REAL;
 ALTER TABLE access ADD COLUMN dest_competition_factor REAL;
@@ -72,15 +77,21 @@ UPDATE access
                         FROM communes WHERE com_id = dest_id );
 
 UPDATE access
-    SET dest_labours = (SELECT POP2009/2
+    SET dest_labours = (SELECT labour
                             FROM communes WHERE com_id = dest_id );
 
-SELECT "Computing destinations jobs and labours";
-UPDATE  access SET time_alpha = ( SELECT exp(Total_Seco*
+SELECT "Computing destinations jobs and labours (alpha)";
+UPDATE  access SET time_alpha = ( SELECT power(Total_Seco,
                                      (SELECT c_val FROM constants
                                           WHERE c_key IS "alpha") )) ;
-UPDATE  access SET dest_weighted_jobs = dest_jobs*time_alpha;
-UPDATE  access SET dest_weighted_labours = dest_labours*time_alpha;
+UPDATE  access SET dest_weighted_jobs = dest_jobs/time_alpha;
+UPDATE  access SET dest_weighted_labours = dest_labours/time_alpha;
+
+SELECT "Computing destinations jobs (beta)";
+UPDATE  access SET time_beta = ( SELECT exp(Total_Seco*
+                                     (SELECT c_val FROM constants
+                                          WHERE c_key IS "beta") )) ;
+UPDATE  access SET dest_weighted_jobs_beta = dest_jobs*time_beta;
 
 SELECT "Computing sum_weighted_jobs for all communes";
 UPDATE communes
@@ -109,18 +120,24 @@ UPDATE access
                                         FROM communes
                                         WHERE com_id = dest_id );
 
-SELECT "Computing accessibilty for all communes";
+SELECT "Computing accessibilty_alpha for all communes";
 UPDATE communes
-    SET accessibility = (SELECT SUM(dest_weighted_jobs*dest_competition_factor)
+    SET accessibility_alpha = (SELECT SUM(dest_weighted_jobs*dest_competition_factor)
                              FROM access
                              WHERE orig_id = com_id
                              AND Total_Seco < (SELECT c_val
                                                    FROM constants
                                                    WHERE c_key IS "Tmax") );
-
+SELECT "Computing accessibilty_beta for all communes";
+UPDATE communes
+    SET accessibility_beta = (SELECT SUM(dest_weighted_jobs_beta)
+                                  FROM access
+                                  WHERE orig_id = com_id
+                              );
 
 /* Dump output to csv*/
 SELECT "Dumping results";
+.header ON
 .separator ","
 .output jobs.csv
 select * from communes;
