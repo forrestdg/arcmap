794460f6529130fe345be5c2c563fb7df76c9abb
Duong Dang <nddang@laas.fr>
2011-01-10 00:40:41 +0100

    Typo in batch.

--- bin/run_hanoi_batch.bat
+++ bin/run_hanoi_batch.bat
@@ -6,33 +6,33 @@ sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\import_hanoi.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\add_columns.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\param_hanoi.sql
 
-sqlite3.exe ..\Data\Hanoi\db.sqlite 'UPDATE constants SET c_val = 0.2 WHERE c_key IN ("alpha", "beta")'
+sqlite3.exe ..\Data\Hanoi\db.sqlite "UPDATE constants SET c_val = 0.2 WHERE c_key IN ('alpha', 'beta')"
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\compute.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\export_hanoi.sql
 move ..\Data\Hanoi\jobs.csv ..\Data\Hanoi\jobs_0.2.csv
 move ..\Data\Hanoi\access.csv ..\Data\Hanoi\access_0.2.csv
 
-sqlite3.exe ..\Data\Hanoi\db.sqlite 'UPDATE constants SET c_val = 0.3 WHERE c_key IN ("alpha", "beta")'
+sqlite3.exe ..\Data\Hanoi\db.sqlite "UPDATE constants SET c_val = 0.3 WHERE c_key IN ('alpha', 'beta')"
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\compute.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\export_hanoi.sql
 move ..\Data\Hanoi\jobs.csv ..\Data\Hanoi\jobs_0.3.csv
 move ..\Data\Hanoi\access.csv ..\Data\Hanoi\access_0.3.csv
 
-sqlite3.exe ..\Data\Hanoi\db.sqlite 'UPDATE constants SET c_val = 0.4 WHERE c_key IN ("alpha", "beta")'
+sqlite3.exe ..\Data\Hanoi\db.sqlite "UPDATE constants SET c_val = 0.4 WHERE c_key IN ('alpha', 'beta')"
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\compute.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\export_hanoi.sql
 move ..\Data\Hanoi\jobs.csv ..\Data\Hanoi\jobs_0.4.csv
 move ..\Data\Hanoi\access.csv ..\Data\Hanoi\access_0.4.csv
 
 
-sqlite3.exe ..\Data\Hanoi\db.sqlite 'UPDATE constants SET c_val = 0.5 WHERE c_key IN ("alpha", "beta")'
+sqlite3.exe ..\Data\Hanoi\db.sqlite "UPDATE constants SET c_val = 0.5 WHERE c_key IN ('alpha', 'beta')"
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\compute.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\export_hanoi.sql
 move ..\Data\Hanoi\jobs.csv ..\Data\Hanoi\jobs_0.5.csv
 move ..\Data\Hanoi\access.csv ..\Data\Hanoi\access_0.5.csv
 
 
-sqlite3.exe ..\Data\Hanoi\db.sqlite 'UPDATE constants SET c_val = 1.0 WHERE c_key IN ("alpha", "beta")'
+sqlite3.exe ..\Data\Hanoi\db.sqlite "UPDATE constants SET c_val = 1.0 WHERE c_key IN ('alpha', 'beta')"
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\compute.sql
 sqlite3.exe ..\Data\Hanoi\db.sqlite <..\scripts\export_hanoi.sql
 move ..\Data\Hanoi\jobs.csv ..\Data\Hanoi\jobs_1.0.csv
