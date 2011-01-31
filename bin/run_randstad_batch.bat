64788e2473492e6a9fe7e6ca362578729c87d2ff
Hong Ha Pham <ph.ha910@gmail.com>
2011-01-31 16:24:19 +0100

    update ha

--- bin/run_randstad_batch.bat
+++ bin/run_randstad_batch.bat
@@ -6,7 +6,7 @@ move ..\Data\Randstad\results\*.csv ..\backup\Randstad\
 SetLocal
 
 set FILES=1-4-2-1-23-5.csv 1-1-2024-1-3-2004.csv
-set ALPHAS=0.2 0.3 0.4 0.5 1.0
+set ALPHAS=0.4
 
 FOR %%f in (%FILES%) do (
 	"C:\Python26\ArcGIS10.0\python.exe" ..\scripts\import_randstad.py %%f
@@ -15,7 +15,7 @@ FOR %%f in (%FILES%) do (
     FOR %%a in (%ALPHAS%) DO (
         sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%a WHERE c_key IN ('alpha', 'beta')"		
 		sqlite3.exe ..\Data\Randstad\db.sqlite "SELECT * FROM constants"
-		::sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\compute.sql
+		sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\compute.sql
 		sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\export_randstad.sql
 		move ..\Data\Randstad\jobs.csv ..\data\Randstad\results\jobs___%%f__%%a.csv
         move ..\Data\Randstad\access.csv ..\data\Randstad\results\access__%%f__%%a.csv
