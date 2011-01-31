99289554154c2a45b8cd7e3eee817438d747226e
Forrest <nddang@laas.fr>
2011-01-31 16:29:49 +0100

    set and print tmax out in filename too

--- bin/run_randstad_batch.bat
+++ bin/run_randstad_batch.bat
@@ -7,18 +7,21 @@ SetLocal
 
 set FILES=1-4-2-1-23-5.csv 1-1-2024-1-3-2004.csv
 set ALPHAS=0.4
-
+set TMAXES=90
 FOR %%f in (%FILES%) do (
-	"C:\Python26\ArcGIS10.0\python.exe" ..\scripts\import_randstad.py %%f
-	sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\add_columns.sql
-	sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\param_randstad.sql
+    "C:\Python26\ArcGIS10.0\python.exe" ..\scripts\import_randstad.py %%f
+    sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\add_columns.sql
+    sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\param_randstad.sql
     FOR %%a in (%ALPHAS%) DO (
-        sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%a WHERE c_key IN ('alpha', 'beta')"		
-		sqlite3.exe ..\Data\Randstad\db.sqlite "SELECT * FROM constants"
-		sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\compute.sql
-		sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\export_randstad.sql
-		move ..\Data\Randstad\jobs.csv ..\data\Randstad\results\jobs___%%f__%%a.csv
-        move ..\Data\Randstad\access.csv ..\data\Randstad\results\access__%%f__%%a.csv
-		)
-	move ..\Data\Randstad\db.sqlite ..\backup\Randstad\db__%%f.sqlite
+        FOR %%t in (%TMAXES%) DO (
+            sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%a WHERE c_key IN ('alpha', 'beta')"		
+            sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%t WHERE c_key IS 'tmax'"		
+	    sqlite3.exe ..\Data\Randstad\db.sqlite "SELECT * FROM constants"
+	    sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\compute.sql
+	    sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\export_randstad.sql
+	    move ..\Data\Randstad\jobs.csv ..\data\Randstad\results\jobs___%%t_%%a_%%f
+            move ..\Data\Randstad\access.csv ..\data\Randstad\results\access__%%t_%%a_%%f
+	)
+    )
+    move ..\Data\Randstad\db.sqlite ..\backup\Randstad\db__%%f.sqlite
 )
