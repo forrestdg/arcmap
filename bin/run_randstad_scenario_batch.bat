66a7a0c13ff54225871ddf39f5da0e29ae349681
Hong Ha Pham <ph.ha910@gmail.com>
2011-02-04 15:23:49 +0100

    Fix typo tmax  -> Tmax

--- bin/run_randstad_scenario_batch.bat
+++ bin/run_randstad_scenario_batch.bat
@@ -7,7 +7,7 @@ SetLocal
 
 set FILES=1-4-2-1-23-5.csv 1-1-2024-1-3-2004.csv
 set ALPHAS=0.4
-set TMAXES=90
+set TMAXES=60 120
 FOR %%f in (%FILES%) do (
     "C:\Python26\ArcGIS10.0\python.exe" ..\scripts\import_randstad_scenario.py %%f
     sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\add_columns.sql
@@ -15,7 +15,7 @@ FOR %%f in (%FILES%) do (
     FOR %%a in (%ALPHAS%) DO (
         FOR %%t in (%TMAXES%) DO (
             sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%a WHERE c_key IN ('alpha', 'beta')"		
-            sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%t WHERE c_key IS 'tmax'"		
+            sqlite3.exe  ..\Data\Randstad\db.sqlite "UPDATE constants SET c_val = %%t WHERE c_key IS 'Tmax'"		
 	    sqlite3.exe ..\Data\Randstad\db.sqlite "SELECT * FROM constants"
 	    sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\compute.sql
 	    sqlite3.exe ..\Data\Randstad\db.sqlite <..\scripts\export_randstad.sql
